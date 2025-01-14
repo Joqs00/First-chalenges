19513c0ce341d8a0fac50b5c961668ff628cae1b
JoX <43328563+Joqs00@users.noreply.github.com>
2025-01-14 09:01:04 -0300

    Add files via upload

---
+++ js-curso-2-desafio_4/app.js
@@ -0,0 +1,27 @@
+//Sétimo e último desafio de lógica de programação em JS
+//Nesta aula foi ensinado sobre listas, nada muito complicado
+//Porém acabei fazendo a aula em um horário fora de costume e não consegui praticar logo depois da aula
+//Então acabei esquecendo como adicionar elementos na lista através de comando e também o comando para chamar um elemento da lista
+//Mas nada que uma pequena pesquisa não resolva
+
+//Primeira etapa:
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//Segunda etapa:
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+
+//Terceira etapa:
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+
+//Quarta etapa:
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Sofia', 'Gabriel', 'Ayumi'];
+
+console.log(listaNomes[0]);//Quarta etapa
+console.log(listaNomes[1]);//Quinta etapa
+console.log(listaNomes[2]);//Sexta etapa
